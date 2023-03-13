723a36e5718dd6f00602f5ce55b2fb4469855fe6
Tarapygin Aleksey <atarapugin@gmail.com>
2023-03-13 14:56:23 +0100

    User avatar uploading routes were added

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const multer = require("multer");
 const router = new express.Router();
 
 const auth = require("../middleware/auth");
@@ -111,5 +112,43 @@ router.delete("/users/me", auth, async (req, res) => {
     res.status(500).send();
   }
 });
+
+const upload = multer({
+  limits: { fileSize: 1000000 },
+  fileFilter: (req, file, cb) => {
+    if (file.originalname.match(/\.(doc|docx)$/)) {
+      return cd(new Error("Please upload a word document!"));
+    }
+
+    cb(undefined, true);
+  },
+});
+
+router.post(
+  "/users/me/avatar",
+  auth,
+  upload.single("avatar"),
+  async (req, res) => {
+    req.user.avatar = req.file.buffer;
+    await req.user.save();
+    res.send();
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
+  }
+);
+
+router.delete(
+  "/users/me/avatar",
+  auth,
+  async (req, res) => {
+    req.user.avatar = undefined;
+    await req.user.save();
+    res.send();
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
+  }
+);
 
 module.exports = router;
